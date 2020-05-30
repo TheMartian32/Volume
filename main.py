cc17dacf72659db9e3ba29940dd352b14366d71f
Justin Buan <justin_b23@icloud.com>
2020-05-30 13:16:19 -0700

    Added comments, doc strings, and got rid of many of the methods in the calc class and renamed it to the ResultsInput class as that was all it was used for.

--- main.py
+++ main.py
@@ -31,7 +31,23 @@ ask = repeat_question()
 
 
 class volume():
+    """Calculates the volume of a given object.
+
+    Returns:
+        Float/Int -- Based off what the user inputs into the program it outputs a floating point number or an integer.
+    """
+
+    # * Setting up parameters that almost every object will need.
     def __init__(self, base=0, height=0, fraction=0, base_length=0, base_height=0):
+        """This is the basic format for just about every object that the vol method will use to calculate volume.
+
+        Keyword Arguments:
+            base {int/float} -- Length of object (default: {0})
+            height {int/float} -- Height of object (default: {0})
+            fraction {int/float} -- If there is a fraction, this is the parameter. (default: {0})
+            base_length {int/float} -- Length of a base (default: {0})
+            base_height {int/float} -- Height of a base (default: {0})
+        """
         self.base = base
         self.height = height
         self.fraction = fraction
@@ -51,6 +67,10 @@ class volume():
         using_frac = ask.ask_for(
             'Are you using a fraction? (Y/N): ', 'Not an answer.', str)
 
+        # * Asks the user if the object is or is not a triangular pyramid.
+        is_tri = ask.ask_for(
+            'Is the object a triangular pyramid? (Y/N): ', 'Not an answer.', str)
+
         # * Inputs to determine the area of a base.
         base_l = self.base_length = ask.ask_for(
             'What is the base length?: ', 'Not a base length', float)
@@ -63,10 +83,6 @@ class volume():
             'What is the height?: ', 'Not a height', float)
 
         print('----------------------------')
-
-        # * Asks the user if the object is or is not a triangular pyramid.
-        is_tri = ask.ask_for(
-            'Is the object a triangular pyramid? (Y/N): ', 'Not an answer.', str)
 
         # * Area of a base
         base_area = base_l*base_h
@@ -81,45 +97,33 @@ class volume():
             # * Asks if the object is a triangle or is not a triangle.
 
             if is_tri[0] == 'y':
+                # using the fraction module, if the user is using a fraction, in which case they have too, it passes the input into a Fraction method.
                 f = Fraction(frac)
+                # * Formula for triangular pyramid
                 volume_calculation = f*0.5*base_area*self.height
-                return volume_calculation
+
+                return print(f'\nThe volume is {volume_calculation} units cubed.')
 
             # * If it is not a triangle, this code executes.
             else:
                 f = Fraction(frac)
+                # * Formula for objects that still require a fractional component.
                 volume_calculation = f*base_area*self.height
-                return volume_calculation
+
+                return print(f'\nThe volume is {volume_calculation} units cubed.')
 
         if using_frac[0] == 'n':
+
+            # * Formula for something like a rectangular prism.
             volume_calculation = base_area*self.height
-            return volume_calculation
+
+            return print(f'\nThe volume is {volume_calculation} units cubed.')
 
 
 v = volume()
 
 
-class calc():
-
-
-    def __init__(self, base_length=0, base_height=0, height=0):
-        self.base_length = base_length
-        self.base_height = base_height
-        self.height = height
-
-    def vol_rect_prism(self):
-
-        volume = v.vol()
-
-        return print(f'\nThe volume is {volume}.')
-
-    def vol_tri_prism(self):
-        volume = v.vol()
-        return print(f'\nThe volume is {volume}.')
-
-    def vol_pyramid(self):
-        volume = v.vol()
-        return print(f'\nThe volume is {volume}.')
+class ResultsInputs():
 
     def calc_type(self):
 
@@ -134,24 +138,24 @@ class calc():
                     'What object are you calculating the volume for?: ', 'Not supported', str)
                 print('----------------------------')
                 if area_type in ['r', 'rect', 'rectangular prism']:
-                    c.vol_rect_prism()
+                    v.vol()
                     break
 
             else:
                 print('Not supported.')
 
 
-c = calc()
+ri = ResultsInputs()
 
 if __name__ == "__main__":
-    c.calc_type()
+    ri.calc_type()
     repeat = ''
     while True:
         # * Asks to repeat the script.
         repeat = input(
             '\nWould you like to repeat the program? (Y/N): ').lower()
         if repeat[0] == 'y'.lower():
-            c.calc_type()
+            ri.calc_type()
             continue
         if repeat[0] == 'n'.lower():
             break
